a07540f3a74995e8a7929e9e662978b9534cb5b2
apple <apple@apples-MacBook-Pro.local>
2020-07-22 12:36:29 +1000

    crated and checked the biosensor

---
+++ biosensor.js
@@ -0,0 +1,25 @@
+// This class generate a living simulator
+  class Biosensor{
+      constructor(name){
+          //we want to initialise with owner and keep track with heart rate 
+         this.owner = name ;
+         this.heartrate = 0 ;
+         this.live();
+      }
+       getRandomArbitrary(min, max) {
+        return Math.random() * (max - min) + min;
+      }
+      // function to generate the heart rate and record it to the sensor
+      //This function generate a random number between 60 -90
+      beat(){
+          let hr=this.getRandomArbitrary(60,70);
+          this.heartrate=hr;
+          console.log('[Owner]:' +this.owner+ '-----[HR] :'+ this.heartrate)
+      }
+      // living function simply starts an endless interval to generate a beat
+      live(){
+          setInterval(()=>{this.beat()},1000)
+      }
+  }
+  let james = new Biosensor("james");
+  //console.log(james)
